ab14f18b7c89b3b3be3c69ab02cb240b8fb2fcc5
MaJeD BoJaN <bojanmajed@gmail.com>
2025-09-29 13:42:55 +0300

    fix the build and layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  layout "authentication"
+
   def new
     @user = User.new
   end
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/presenters/**/*.rb',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
